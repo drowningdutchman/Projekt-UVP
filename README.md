00c65ecc2e39191bf97f9d1d9bc2413cf9ee54e2
Lovro <petekl1@proton.me>
2024-08-09 15:29:03 +0200

    omejitev podatkov

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Analiza podatkov o stripih 
+# Analiza podatkov o japonskih stripih zadnjih 5 let 
 ## Projektna naloga pri predmetu uvod v programiranje. 
-Odlocil sem se analizirati podatke o mangah s spletne strani mangafire, ki jo najpogosteje uporabljam. Upam, da mi bo projekt v prihodnje pomagal pri lazjem odlocanju za dolocen strip:).
+Odlocil sem se analizirati podatke o mangah s spletne strani mangafire, ki jo najpogosteje uporabljam. Da bi nekoliko omejil kolicino podatkov, sem se odlocil omejiti na tiste, ki so izhajali od leta 2020 dalje - priblizno 3000 stripov. Upam, da mi bo projekt v prihodnje pomagal pri lazjem odlocanju za branje:).
 ## Vsebina
--- pridobi_html.py
+++ pridobi_html.py
@@ -1,17 +1,24 @@
 import requests
+import os
 
-
-mojurl = 'https://mangafire.to/type/manga'
-mojime = 'mangafire.html'
-
-
-def pridobi_html(url, filename):
-    #pridobimo html
+def pridobi_html(url, directory, filename):
+    #pridobimo html in nastavimo parametre
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
     vsebina = requests.get(url).text
-
+    
     #sedaj shranimo v datoteko
-    with open(filename, 'w', encoding='utf-8') as izhodna:
+    with open(path, 'w', encoding='utf-8') as izhodna:
         izhodna.write(vsebina)
     return None
 
-pridobi_html(mojurl, mojime)
+def pridobi_htmlje(st_strani):
+    for i in range(1, st_strani):
+        mojurl = f"https://mangafire.to/filter?keyword=&type%5B0%5D=manga&status%5B0%5D=completed&year%5B0%5D=2024&year%5B1%5D=2023&year%5B2%5D=2022&year%5B3%5D=2021&year%5B4%5D=2020&sort=recently_updated&page={i}"
+        mojdir = 'HTML-ji'
+        mojime = f"mangafire{i}.html"
+
+        pridobi_html(mojurl, mojdir, mojime)
+    return None
+
+pridobi_htmlje(95)
